bd73401e9bcc668bb48f4e577e7652c9a4e65cc9
jaka2005 <jakalogin1808@gmail.com>
2024-02-17 22:18:08 +0300

    change lib for working with tg api

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-import asyncio
 import time
-from aiogram import Bot
 import schedule
+from telebot import TeleBot
+from telebot.util import quick_markup
+
 from src.config import CATEGORY, CHAT_ID, PERIOD, TOKEN
 from src.kwork_parser import NEW_OFFER_URL, Kworks, get_new_kworks
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
 
 
 MESSAGE_TEXT = """
@@ -15,27 +15,24 @@ MESSAGE_TEXT = """
 """
 
 
-async def send_kowrks(bot: Bot, kworks: Kworks):
+async def send_kowrks(bot: TeleBot, kworks: Kworks):
     for id, kwork in kworks.items():
         await bot.send_message(CHAT_ID, MESSAGE_TEXT.format(
             title=kwork.title,
             price=kwork.price,
             description=kwork.description
-        ), reply_markup=InlineKeyboardMarkup(inline_keyboard=[
-            [InlineKeyboardButton(
-                text="Откликнуться",
-                url=NEW_OFFER_URL.format(id=id)
-            )]
-        ]))
-        await asyncio.sleep(1/3)
+        ), reply_markup=quick_markup({
+            "Откликнуться": {"url": NEW_OFFER_URL.format(id=id)}
+        }))
+        time.sleep(1/3)
 
 
-bot = Bot(token=TOKEN)
+bot = TeleBot(token=TOKEN)
 
 
 def main():
     kworks = get_new_kworks(CATEGORY)
-    asyncio.run(send_kowrks(bot, kworks))
+    send_kowrks(bot, kworks)
 
 
 if __name__ == "__main__":
