7fd479ce3abf55ee889465188ac122500ab1d3ad
jaka2005 <94551033+jaka2005@users.noreply.github.com>
2024-03-22 21:08:06 +0300

    fix: added model_config to work specifically with Dotenv

--- src/config.py
+++ src/config.py
@@ -1,14 +1,16 @@
 from functools import lru_cache
 
 from pydantic import Field
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Config(BaseSettings):
     """
-    Environment settings for the bot
+    `.env` settings for the bot
     """
 
+    model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
+    
     db_connection_url: str = Field(default=...)
     token: str = Field(default=...)
     category: int = Field(default=...)
